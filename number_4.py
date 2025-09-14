667f3ba80a669e0ccbdc15e76b08affaf6a6665c
WoobinHwang <dnqls0326@naver.com>
2025-09-14 21:27:42 +0900

    pass??

--- number_4.py
+++ number_4.py
@@ -1,4 +1,6 @@
-from flask import Flask   # flask 클래스를 가져온다.
+from flask import Flask, request
+
+
 app = Flask(__name__)     # 플라스크 객체를 생성한다. __name__은 현재 실행 중인 모듈 이름을 전달하는 것이다.
 
 @app.route('/')           # 기본서버 127.0.0.1:5000 뒤에 붙는 주소를 적어준다.
@@ -20,6 +22,32 @@ def hello():
                 {
                     "simpleText": {
                         "text": "시작하셔도 됩니다."
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+# 유저가 입력 한 값 반환
+@app.route('/api/test', methods=['POST'])
+def test():
+    body = request.get_json() # 사용자가 입력한 데이터
+
+    # 입력값 줄바꿈 제거
+    body2 = str(body['userRequest']['utterance']).strip()
+    userID = str(body['userRequest']['user']['id'])
+
+
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": body2 + "입력완료!  \n" + userID
                     }
                 }
             ]
